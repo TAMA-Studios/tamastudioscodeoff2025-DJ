986a9350d974acd9b98de4b7dfb9d1c445c97497
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2025-11-06 01:15:53 -0600

    been 15 minutes ig. continues on the projects loader

--- projectloader.js
+++ projectloader.js
@@ -1,3 +1,44 @@
 async function loadProjects() {
-    const container = document.getElementById("projects")
+    const container = document.getElementById("projects");
+    if (!container) return;
+
+    try {
+        // get projects json file
+        const response = await fetch("projects.json");
+        if (!response.ok) throw new Error("failed to uh. get the projects.");
+        const projects = await response.json();
+
+
+        // clear div
+        container.innerHTML = "";
+
+        // build projects
+        projects.forEach(project => {
+            const card = document.createElement("div");
+            card.className = "projectscard";
+
+
+            const link = document.createElement("a");
+            link.href = project.url;
+            link.target = "_blank";
+            link.rel = "noopener norefferer";
+
+
+            const title = document.createElement("h3");
+            title.textContent = project.className;
+
+            const desc = document.createElement("p");
+            desc.textContent = project.description;
+
+
+            link.appendChild(title);
+            link.appendChild(desc);
+
+            if (project.notice) {
+                const notice = document.createElement("p");
+                notice.className = "notice";
+                Notic
+            }
+        });
+    }
 }
