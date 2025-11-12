a454dfb691d143f5c3445273ebf7498bd06cb167
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2025-11-12 16:33:06 -0600

    eggs and more eggs

--- boring-stuff.js
+++ boring-stuff.js
@@ -1,4 +1,15 @@
-    function santa(){
-        document.getElementById("copyright").innerHTML = "<p>&copy; TAMA Studios 2025. Happy hollidays!</p>"
-        console.log("You found an easter egg! Many more to find. Check the copyright")
-    }
+    let codegegg1 = false
+    function santaegg(){
+        document.getElementById("copyright").innerHTML = "<p>&copy; TAMA Studios 2025. Happy hollidays!</p>";
+        console.log("You found an easter egg! Many more to find. Check the copyright");
+    };
+    function codeguyegg() {
+        if (codegegg1 === false) {
+            console.error("Try again bud :]");
+            return;
+        };
+        const codeegg = document.createElement("h1");
+        const eggdiv = document.getElementById("section1");
+        codeegg.innerHTML = "Wow you founf this easter egg. got some more to find tho ;]"
+        eggdiv.appendChild(codeegg)
+    };
